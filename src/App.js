15a2f80f046bb5fbb5368fd23f49ec86e2bcd6a2
Daniel-Shliakhetko <danielshliakhetko@gmail.com>
2023-08-20 18:34:34 +0300

    Add Knowledge

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { AboutMe } from "./sections/AboutMe/AboutMe";
 import { Introduction } from "./sections/Introduction/Introduction";
+import { Knowledge } from "./sections/Knowledge/Knowledge";
 
 function App() {
   return (
     <div className="App">
       <div className="Wrapper">
         <Introduction />
-        <AboutMe/>
+        <AboutMe />
+        <Knowledge />
       </div>
     </div>
   );
---
+++ src/sections/Knowledge/Knowledge.js
@@ -0,0 +1,17 @@
+import React from "react";
+import SectionTitle from "../../components/titles/SectionTitle/SectionTitle";
+import TechnologyStack from "../../components/cards/TechnologyStack/TechnologyStack";
+import technologies from "../../data/technologies";
+import "./index.scss";
+import Section from "../Section";
+
+export const Knowledge = () => {
+  return (
+    <Section className="Knowledge">
+      <SectionTitle textAlign="Center" additional="Stack">
+        Technology{" "}
+      </SectionTitle>
+      <TechnologyStack cards={technologies} max={8}/>
+    </Section>
+  );
+};
