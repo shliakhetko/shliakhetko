7be15b2249cf5a1d981a1bfa4a3a9befb9b6bc50
Daniel Shliakhetko <danielshliakhetko@gmail.com>
2023-12-24 00:05:21 +0200

    Add social media in Footer

--- src/sections/Footer/Footer.js
+++ src/sections/Footer/Footer.js
@@ -2,13 +2,15 @@ import React from "react";
 import Section from "../Section";
 import "./index.scss";
 import FooterLinks, { links } from "./FooterLinks";
-import FooterContacs from "./FooterContacs";
+// import FooterContacs from "./FooterContacs";
 import FooterCopyright from "./FooterCopyright";
+import socialNetworks from "../../data/socialNetworks";
 
 export const Footer = () => {
     return (<Section className="Footer">
         <div className="Footer__Container">
-            <FooterLinks list={links} />
+            <FooterLinks title={"Navigation"} list={links} />
+            <FooterLinks title={"Social Media"} list={socialNetworks} targetBlank={true} />
         </div>
         <FooterCopyright />
     </Section>)
--- src/sections/Footer/FooterLinks.js
+++ src/sections/Footer/FooterLinks.js
@@ -17,25 +17,28 @@ export const links = [{
 
 export const FooterLinks = (props) => {
     return <div className="FooterLinks">
-        <span className="FooterLinks__Title">Navigation</span>
+        <span className="FooterLinks__Title">{props.title}</span>
         <ul className="FooterLinks__Menu">
-            {props.list.map((item, i) => <FooterLink content={item} />)}
+            {props.list.map((item, i) => <FooterLink content={item} targetBlank={props.targetBlank} />)}
         </ul>
     </div>
 }
 
 FooterLinks.propTypes = {
+    title: PropTypes.string,
     list: PropTypes.array,
+    targetBlank: PropTypes.bool,
 }
 
 const FooterLink = (props) => {
     const content = props.content;
 
-    return <li className="FooterLinks__Item"><a className="FooterLinks__Link" href={content.link}>{content.title}</a></li>
+    return <li className="FooterLinks__Item"><a className="FooterLinks__Link" href={content.link} target={props.targetBlank && "_blank"} rel="noreferrer">{content.title}</a></li>
 }
 
 FooterLink.propTypes = {
     content: PropTypes.object,
+    targetBlank: PropTypes.bool,
 }
 
 export default FooterLinks;
